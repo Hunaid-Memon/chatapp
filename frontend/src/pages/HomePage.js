a4b5fcdddec1b58604b2629c473e4a6a29b8a888
Hunaid <hunaid.hanif@gmail.com>
2022-07-13 00:20:23 +0500

    Sign up & Login form

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
+import Login from '../components/authentication/Login';
+import Signup from '../components/authentication/Signup';
+import { Container, Box, Text } from '@chakra-ui/react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p="3"
+        bg="transparent"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+        textAlign="center"
+      >
+        <Text fontSize="4xl" fontFamily="work sans" color="black">
+          Chat App
+        </Text>
+      </Box>
+      <Box
+        p="4"
+        bg="transparent"
+        w="100%"
+        borderRadius="lg"
+        borderWidth="1px"
+        textAlign="center"
+        color="black"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab w="50%" color="black">
+              Login
+            </Tab>
+            <Tab w="50%" color="black">
+              Sign Up
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 }
 
 export default HomePage
